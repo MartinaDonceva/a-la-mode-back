1507c3361eb8a28fcb6d493f7487cce1481a12b6
MartinaDonceva <doncevamartina@yahoo.com>
2023-01-29 17:05:31 +0100

    CRUD operations for Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,5 +8,36 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductController extends Controller
 {
-//
+    public function index(){
+        $products = Product::query()->paginate(5);
+        return JsonResource::collection($products);
+    }
+
+    public function store(Request $request) {
+        // logic for creating and validating the request
+        $fields = $request->validate([
+            'name' => 'required',
+            'price' => 'required',
+            'description' => 'required',
+            'image' => 'required'
+        ]);
+        Product::query()->create($fields);
+        return redirect()->back();
+    }
+
+    public function update(Product $product, Request $request){
+        $fields = $request->validate([
+            'name' => 'required',
+            'price' => 'required',
+            'description' => 'required',
+            'image' => 'required'
+        ]);
+        Product::query()->update($fields);
+        return redirect()->back();
+    }
+
+    public function destroy(Product $product){
+        $product->delete();
+        return redirect()->back();
+    }
 }
--- database/migrations/2023_01_29_144748_create_products_table.php
+++ database/migrations/2023_01_29_144748_create_products_table.php
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+            $table->unsignedInteger('price');
+            $table->text('description');
+            $table->string('image');
         });
     }
 
